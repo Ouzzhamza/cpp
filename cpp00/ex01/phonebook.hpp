c801bc3f735ffd4b058bd4e457234ad4d2e7f9c5
Hamza Ouazzani <houazzan@e2r2p15.1337.ma>
2022-05-24 12:34:09 +0100

    phon_book

---
+++ cpp00/ex01/phonebook.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: houazzan <houazzan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/24 11:45:38 by houazzan          #+#    #+#             */
+/*   Updated: 2022/05/24 11:45:38 by houazzan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#if !defined(PHONE_BOOK_HPP)
+#define PHONE_BOOK_HPP
+
+class phonebook
+{
+private:
+    /* data */
+public:
+    phonebook(/* args */);
+    ~phonebook();
+};
+
+phonebook::phonebook(/* args */)
+{
+}
+
+phonebook::~phonebook()
+{
+}
+
+
+
+#endif // PHONE_BOOK_HPP
