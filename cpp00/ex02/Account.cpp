c2cccb445400256faf109f75921960793bfff741
Hamza Ouazzani <houazzan@student.1337.ma>
2022-09-14 21:22:47 +0100

    order in distruct

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: houazzan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 10:09:53 by houazzan          #+#    #+#             */
-/*   Updated: 2022/09/13 18:50:51 by houazzan         ###   ########.fr       */
+/*   Updated: 2022/09/14 21:20:20 by houazzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,112 @@
 #include <ctime>
 #include "Account.hpp"
 
-static int  getNbAccounts( void )
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+
+int  Account::getNbAccounts( void )
 {
-	return(accountIndex);
+	return(_nbAccounts);
 }
 
-static int  getTotalAmount( void )
+int  Account::getTotalAmount( void )
 {
-	return (amount);
+	return (_totalAmount);
 }
-void _displayTimestamp(void)
+
+int Account::getNbDeposits(void)
+{
+	return(_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals(void)
+{
+	return(_totalNbWithdrawals);
+}
+
+Account::Account(int initial_diposit)
+{
+    _displayTimestamp();
+	_accountIndex = _nbAccounts++;
+    _amount = initial_diposit;
+   _totalAmount += _amount;	
+    std::cout << "index:"   << _accountIndex
+              << ";amount:" << _amount
+              << ";created" << std::endl;
+}
+
+Account::~Account(void)
+{
+	_displayTimestamp();
+	std::cout << "index:"   << _accountIndex
+			  << ";amount:" << _amount
+			  << ";closed"  << std::endl;
+}
+void Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+	_nbDeposits++;
+	std::cout << "index:"        << _accountIndex
+			  << ";p_amount:"    << _amount
+			  << ";diposit:"     << deposit
+			  << ";amount:"       << _amount + deposit
+			  << ";nb_deposits:" <<  _nbDeposits << std::endl;
+	_amount += deposit;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+}
+
+void Account::displayAccountsInfos(void)
+{
+	_displayTimestamp();
+	std::cout << "accounts:"   << getNbAccounts()
+		      << ";total:"     << getTotalAmount()
+			  << ";diposits:"  << getNbDeposits()
+			  << ";withdrawals:"<< getNbWithdrawals()  << std::endl;
+}
+
+
+void Account::displayStatus() const
+{
+	_displayTimestamp();
+    std::cout << "index:"      << _accountIndex
+              << ";amount:"    << _amount
+              << ";diposits:"  << _nbDeposits
+              << ";withdrawals:"<< _nbWithdrawals << std::endl;
+}
+
+bool    Account::makeWithdrawal( int withdrawal )
+{
+	_displayTimestamp();
+	if (_amount < withdrawal)
+	{
+		std::cout << "index:"  << _accountIndex
+				  << ";p_amount:" << _amount
+				  << ";withdrawal:refused" << std::endl;
+	}
+	else
+	{
+		_nbWithdrawals++;
+        std::cout << "index:"  << _accountIndex
+				  << ";p_amount:" << _amount
+				  << ";withdrawal:" << withdrawal
+				  << ";amount:" << _amount - withdrawal 
+				  << "nb_withdrawals:" << _nbWithdrawals << std::endl;
+		_amount -= withdrawal;
+		_totalAmount -= withdrawal;
+		_totalNbWithdrawals++;
+	}
+	return(0);
+
+}
+
+void Account::_displayTimestamp(void)
 {
 	char str[20];
 	std::time_t timestamp = std::time(NULL);
 	std::strftime (str, 20, "%Y%m%d_%H%M%S", std::localtime(&timestamp));
-	std::cout << "[" << str << "]" << std::endl;
-}
-
-void    displayStatus( void ) const
-{
-	_displayTimestamp;
+	std::cout << "[" << str << "] ";
 }
--- cpp00/ex02/tests.cpp
+++ cpp00/ex02/tests.cpp
@@ -8,6 +8,7 @@
 // ************************************************************************** //
 
 #include <vector>
+#include <iostream>
 #include <algorithm>
 #include <functional>
 #include "Account.hpp"
@@ -24,7 +25,6 @@ int		main( void ) {
 	accounts_t				accounts( amounts, amounts + amounts_size );
 	accounts_t::iterator	acc_begin	= accounts.begin();
 	accounts_t::iterator	acc_end		= accounts.end();
-
 	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };
 	size_t const		d_size( sizeof(d) / sizeof(int) );
 	ints_t				deposits( d, d + d_size );
