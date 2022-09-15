4d8a82881daf4c696688a9b8e77a2f9e9b755258
Hamza Ouazzani <houazzan@student.1337.ma>
2022-09-15 19:40:15 +0100

    starting module 2

---
+++ CPP_Module_01/ex00/Zombie.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: houazzan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/15 12:55:36 by houazzan          #+#    #+#             */
+/*   Updated: 2022/09/15 18:57:40 by houazzan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+
+class Zombie
+{
+	std::string name = Zombie.name;
+	public:
+	void announce (void);
+};
+
+#endif 
